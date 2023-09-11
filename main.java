3a7570aef1e0a961ba12d4dc1dca01a570611baf
Antônio <137973384+toedas@users.noreply.github.com>
2023-09-10 22:28:47 -0300

    Minha primeira alteração

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Olá Mundo");
+
+
+        
+        teclado.close();
+    }
+}
